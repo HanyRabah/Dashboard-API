66d31a55194515329f757455024f8180af61a6ce
Hany <hany.rabah@gmail.com>
2020-07-25 13:44:30 +0400

    adding prod config

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,7 @@
 module.exports = {
   DB_URI: process.env.DB_URI,
   AUTH_NAMESPACE: process.env.AUTH_NAMESPACE,
+  JWKSURI: process.env.JWKSURI,
+  JWT_AUDIENCE: process.env.JWT_AUDIENCE,
+  JWT_ISSUER: process.env.JWT_ISSUER,
 }
